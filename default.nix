78005d6e2c1396a0571eb61e4a473d4590cdcf4c
Eric Harris-Braun <eric@harris-braun.com>
2022-03-12 12:14:07 -0500

    WIP initial directory structure

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  holonixRev = "9ddfc68aaf665f13e674a5382946f2e262538abe";
+  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/${holonixRev}.tar.gz";
+  holonix = import (holonixPath) {
+    holochainVersionId = "v0_0_125";
+  };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  packages = with nixpkgs; [
+    # Additional packages go here
+    nodejs-16_x
+  ];
+}
